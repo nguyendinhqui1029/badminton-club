3fa1f5f95bbd12cc7013cf69bf0f9a1aae3314fa
NguyenQui <nguyendinhqui1029@gmail.com>
2024-08-21 10:13:15 +0700

    Update environment.ts

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -2,6 +2,6 @@ import { Environment } from "@app/models/environment.model";
 
 export const environment: Environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api/v1',
+    apiUrl: 'https://badminton-club-fqk6.onrender.com/api/v1',
     defaultLanguage: 'vi',
 };
